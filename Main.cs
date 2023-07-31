94fce9777e432307a7c31f99d83b96f2acb6696b
Alex Arias <alex.arias.jorym@gmail.com>
2023-07-31 22:11:37 +0800

    feat: add scoring and credits functionality

--- Main.cs
+++ Main.cs
@@ -7,6 +7,8 @@ namespace SlotGameMachine
     {
         Image[] imageArray;
         PictureBox[] pictureBoxes;
+        int score = 0;
+        int credit = 100;
 
         public Main()
         {
@@ -20,6 +22,8 @@ namespace SlotGameMachine
                 Image.FromFile("assets/coin.png")
             };
             pictureBoxes = new PictureBox[3] { picSlot1, picSlot2, picSlot3 };
+
+            updateLabels();
         }
 
         private void setPictureBoxImage(PictureBox pictureBox)
@@ -32,30 +36,79 @@ namespace SlotGameMachine
             }
         }
 
+        private void updateLabels()
+        {
+            lblScoreValue.Text = score.ToString();
+            lblCreditValue.Text = credit.ToString();
+        }
+
         private int creditsWon()
         {
             if (pictureBoxes != null)
             {
-                int distinctCount = pictureBoxes.Distinct().Count();
+                int distinctCount = pictureBoxes.Distinct(new PictureBoxImageComparer()).Count();
 
                 switch (distinctCount)
                 {
                     case 1:
+                        Debug.WriteLine("1");
                         return 30;
                     case 2:
+                        Debug.WriteLine("2");
                         return 10;
                 }
             }
-
             return 0;
         }
 
         private void btnSpin_Click(object sender, EventArgs e)
         {
-            foreach (PictureBox pictureBox in pictureBoxes)
+            if (credit > 0)
             {
-                setPictureBoxImage(pictureBox);
+                credit -= 10;
+                foreach (PictureBox pictureBox in pictureBoxes)
+                {
+                    setPictureBoxImage(pictureBox);
+                }
+
+                credit += creditsWon();
+                score += creditsWon();
+
+                updateLabels();
             }
+            else
+            {
+                lblResult.Text = "You don't have enough credits!";
+                centerLabel(lblResult);
+            }
+        }
+
+        private void centerLabel(Label label)
+        {
+            label.Left = (this.ClientSize.Width - label.Width) / 2;
+        }
+    }
+
+    /// <summary>
+    /// Custom comparer to compare PictureBox objects based on the images they display
+    /// </summary>
+    public class PictureBoxImageComparer : IEqualityComparer<PictureBox>
+    {
+        public bool Equals(PictureBox x, PictureBox y)
+        {
+            // Compare images if both PictureBoxes have images
+            if (x?.Image != null && y?.Image != null)
+            {
+                return x.Image.Equals(y.Image);
+            }
+
+            // If any PictureBox is null or doesn't have an image, consider them not equal
+            return false;
+        }
+
+        public int GetHashCode(PictureBox obj)
+        {
+            return obj?.Image?.GetHashCode() ?? 0;
         }
     }
 }
